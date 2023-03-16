1443a7f91a113f9fee98dde0d22735b309b367f2
Keri van der Westhuizen <75972814+kerivdw@users.noreply.github.com>
2023-03-16 16:08:19 +1300

    create interfaces

--- client/data/meme.ts
+++ client/data/meme.ts
@@ -1,3 +1,17 @@
-interface Memes{
-  
+interface Image {
+  id: number,
+  name: string,
+  link: string,
+  comment: string [],
 }
+
+interface Memes{ 
+  images: Image []
+}
+
+const memes : Memes  = {images: [
+  {id: 1, name: 'string', link: 'string', comment:  ['']  }, 
+  {id: 2, name: 'string', link: 'string', comment:  ['']  }, ]}
+
+
+// https://github.com/tohora-2023/worldwide-routing/blob/ahmad/data/continents.ts
