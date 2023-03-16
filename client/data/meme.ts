6bc6f98b797ec2f5296da45b5ce720aba81700ac
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-03-16 16:18:38 +1300

    data meme built

--- client/data/meme.ts
+++ client/data/meme.ts
@@ -10,11 +10,18 @@ interface Memes{
 }
 
 const memes : Memes  = {images: [
-  {id: 1, name: 'string', link: 'string', comment:  ['']  }, 
-  {id: 2, name: 'string', link: 'string', comment:  ['']  }, ]}
+  {id: 1, name: 'squint', link: 'img-1.gif', comment:  ['']  }, 
+  {id: 2, name: 'john travolta', link: 'img-2.gif', comment:  ['']  },
+  {id: 3, name: 'monkey', link: 'img-3.gif', comment:  ['']  },
+  {id: 4, name: 'sad-cat', link: 'img-4.png', comment:  ['']  },
+  {id: 5, name: 'unimpressed-guy', link: 'img-5.png', comment:  ['']  },
+  {id: 6, name: 'burning-computers', link: 'img-6.png', comment:  ['']  },
+  {id: 7, name: 'swing-man', link: 'img-7.gif', comment:  ['']  },
+  {id: 8, name: 'pizza', link: 'img-8.gif', comment:  ['']  },
+  {id: 9, name: 'kermit-typing', link: 'img-9.gif', comment:  ['']  },
+  {id: 10, name: 'office-cringe', link: 'img-10.gif', comment:  [''] },
+  {id: 11, name: 'rolling-eyes', link: 'img-11.gif', comment:  [''] }]}
 
 
   
 export default memes
-
-// https://github.com/tohora-2023/worldwide-routing/blob/ahmad/data/continents.ts
