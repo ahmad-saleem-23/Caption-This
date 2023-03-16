621b6d8bce7e8609baee05383f6252c1527bc55d
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-03-17 12:18:23 +1300

    add meme generator

--- client/components/Meme.tsx
+++ client/components/Meme.tsx
@@ -1,47 +1,67 @@
 import React, { useState } from 'react'
 import memes from '../data/meme'
 
+interface Image {
+  id: number
+  name: string
+  link: string
+  comment: string[]
+}
+
+interface Memes {
+  images: Image[]
+}
 
 export default function Meme() {
+  // const [Memes, setMemes] = useState(Memes)
+  const [img, setimg] = useState(generateMeme())
+  // const [imgData, setImageData] = useState({})
+  // const[text, setText] = useState('')
+
   function generateMeme() {
     const randMemeIndex = Math.floor(Math.random() * memes.images.length)
     const image = memes.images[randMemeIndex]
-    setimg(image)
-    
+    return image
   }
 
-  function displayRandImg(){
-    return <img src={`./images/${img.link}`} alt={img.name} />
-  }
+  //get a random image
+  //save the image to state (save the id in state)
+  // show the image
+  // get the image id from state to add a comment
 
-  const [Memes, setMemes] = useState(Memes)
-  const [img, setimg] = useState(generateMeme())
-  const[text, setText] = useState('')
+  const currentImg = function displayRandImg() {
+        return (
+      <>
+        <img src={`./images/${img.link}`} alt={img.name} />
+      </>
+    )
+  }
 
   function handleGenerate() {
     const newImg = generateMeme()
+    console.log(newImg)
     setimg(newImg)
   }
-  function handlechange(event:React.ChangeEvent<HTMLInputElement>) {
-    event.preventDefault(); 
-    const comment=event.target.value  
-    setText(comment)
-  }
-  function handelSubmit(){
-   const newComment= Memes.images.map((meme)=>{
-    if ( img.id === meme.id){
 
-    }
-   })
-  }
+  // function handlechange(event:React.ChangeEvent<HTMLInputElement>) {
+  //   event.preventDefault();
+  //   const comment=event.target.value
+  //   setText(comment)
+  // }
+  // function handelSubmit(){
+  //  const newComment= Memes.images.map((meme)=>{
+  //   if ( img.id === meme.id){
+
+  //   }
+  //  })
+  // }
 
   return (
     <>
       <div className="meme-container">
-        {img}
-
+        <>{currentImg()}</>
         <div>
-          <form onSubmit={handelSubmit}>
+          {/* <form onSubmit={handelSubmit}>
           
           <input type="text" 
             name='comment'           
@@ -49,9 +69,12 @@ export default function Meme() {
             onChange={handlechange}/>
             <button  type='submit'>  Submit CaptionReact
             </button>
-          </form>
+          </form> */}
 
-          <button className="meme-button generate-button" onClick={handleGenerate}>
+          <button
+            className="meme-button generate-button"
+            onClick={handleGenerate}
+          >
             Generate Image
           </button>
         </div>
@@ -59,6 +82,5 @@ export default function Meme() {
     </>
   )
 }
-
 
 //https://github.com/tohora-2023/code-from-class/blob/main/week4/wed-pm/1-two-inputs/client/components/AddAnimalForm.tsx
