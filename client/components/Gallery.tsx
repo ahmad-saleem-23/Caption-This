fa494c612a148f32b641ef29e271ead94e8af786
Keri van der Westhuizen <75972814+kerivdw@users.noreply.github.com>
2023-03-17 13:38:54 +1300

    add gallery view

--- client/components/Gallery.tsx
+++ client/components/Gallery.tsx
@@ -1,10 +1,42 @@
-export default function Gallery() {
+import React, { useState } from 'react'
+import Memes from '../data/meme'
 
+//get a list of images
+//display the images to the grid
+//display comments
+
+//Stretch
+//create a link back to the meme page
+//http://localhost:3000/captionthis/:id
+
+interface Image {
+  id: number
+  name: string
+  link: string
+  comment: string[]
+}
+
+interface Memes {
+  images: Images[]
+}
+
+export default function Gallery() {
+  const [memeData, setMemes] = useState(Memes)
 
   return (
     <div>
       <h2>Gallery</h2>
-      <p>Check out all the memes!</p>
+      <div>
+        {memeData.images.map((image: Image) => {
+          return (
+            <img
+              key={image.id}
+              src={`./images/${image.link}`}
+              alt={image.name}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
