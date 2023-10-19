e1597ef78f0e8cf76f80f47c194993069c0b2b31
Ahmad Saleem <ahmadsaleem94@hotmail.com>
2023-10-19 18:14:41 +1300

    adjust css for home page

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -4,8 +4,10 @@ export default function Home() {
       <div className="w-full bg-green-400 p-[20px]">
         <h2 className="text-2xl text-center font-bold p-2">Home</h2>
       </div>
-      <div className="w-full flex justify-center items-center p-20">
-        <p>
+    
+      <div className="w-full flex justify-center items-center p-20  ">
+        <p className="bg-green-100 p-[10px]
+        rounded-2xl">
           Welcome to Caption This! To play, click on Caption This in the Nav Bar.
           All you have to do is add a caption to one of our fabulous memes! You
           can view other peoples captions in the gallery page! The caption with
